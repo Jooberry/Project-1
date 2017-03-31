8ca96dd3302b6d53c79b00fd8ba04bc6e2ce62c8
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-03-31 18:41:16 +0100

    created my database tables

---
+++ plushie_shop/db/plushie_shop.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS plushies;
+DROP TABLE IF EXISTS brands;
+
+CREATE TABLE brands (
+  id SERIAL4 PRIMARY KEY,
+  brand_name VARCHAR(255)
+);
+
+CREATE TABLE plushies (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  brand_id INT4 REFERENCES brands(id) ON DELETE CASCADE
+);
