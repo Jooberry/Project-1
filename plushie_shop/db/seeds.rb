d10a6b2e7f43e59bdd749efc5bb7d82bdd302223
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-04-03 10:12:03 +0100

    added seeds to seeds.rb file

--- plushie_shop/db/seeds.rb
+++ plushie_shop/db/seeds.rb
@@ -16,3 +16,27 @@ brand2 = Brand.new({
 brand1.save()
 brand2.save()
 
+plushie1 = Plushie.new({
+  "name" => "Rilakkuma",
+  "brand_id" => brand1.id
+  })
+
+plushie2 = Plushie.new({
+  "name" => "Badtz-maru",
+  "brand_id" => brand2.id
+  })
+
+plushie3 = Plushie.new({
+  "name" => "Cinnamoroll",
+  "brand_id" => brand2.id
+  })
+
+plushie4 = Plushie.new({
+  "name" => "Gudetama",
+  "brand_id" => brand2.id
+  })
+
+plushie1.save()
+plushie2.save()
+plushie3.save()
+plushie4.save()
