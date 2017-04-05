8f5acec7a86461cdd5078e5dd2fe06f6f5d246a1
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-04-05 09:22:53 +0100

    fixed apostrophe in description bug and name in edit brand form

--- plushie_shop/models/brand.rb
+++ plushie_shop/models/brand.rb
@@ -8,10 +8,11 @@ class Brand
     @id = options['id'].to_i
     @brand_name = options['brand_name']
     @picture = options['picture']
-    @description = options['description'].gsub("'", "''")
+    @description = options['description'].gsub("''", "'")
   end
 
   def save()
+    @description = @description.gsub("'", "''")
     sql = "INSERT INTO brands (brand_name, picture, description) VALUES ('#{@brand_name}', '#{@picture}', '#{@description}') RETURNING *"
     brand_data = SqlRunner.run(sql)
     @id = brand_data.first()['id'].to_i
@@ -24,6 +25,7 @@ class Brand
   end
 
   def update()
+    @description = @description.gsub("'", "''")
     sql = "UPDATE brands SET (
         brand_name, picture, description) = ( 
         '#{@brand_name}', '#{@picture}', '#{@description}')
